326b4317599616a49fdd0b944b7ea95cc2900780
Yuri Kovalov <yuri@yurikoval.com>
2013-01-04 14:24:36 +0900

    Use 'f' for 'form'

--- serialize.js
+++ serialize.js
@@ -1,32 +1,32 @@
-window.serialize = function(form) {
+window.serialize = function(f) {
   var i, j, q;
-  if (!form || form.nodeName !== "FORM") {
+  if (!f || f.nodeName !== "FORM") {
     return;
   }
   i = void 0;
   j = void 0;
   q = [];
-  i = form.elements.length - 1;
+  i = f.elements.length - 1;
   while (i >= 0) {
-    if (form.elements[i].name === "") {
+    if (f.elements[i].name === "") {
       i = i - 1;
       continue;
     }
-    switch (form.elements[i].nodeName) {
+    switch (f.elements[i].nodeName) {
       case "INPUT":
-        switch (form.elements[i].type) {
+        switch (f.elements[i].type) {
           case "text":
           case "hidden":
           case "password":
           case "button":
           case "reset":
           case "submit":
-            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+            q.push(f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value));
             break;
           case "checkbox":
           case "radio":
-            if (form.elements[i].checked) {
-              q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+            if (f.elements[i].checked) {
+              q.push(f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value));
             }
             break;
           case "file":
@@ -34,29 +34,29 @@ window.serialize = function(form) {
         }
         break;
       case "TEXTAREA":
-        q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+        q.push(f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value));
         break;
       case "SELECT":
-        switch (form.elements[i].type) {
+        switch (f.elements[i].type) {
           case "select-one":
-            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+            q.push(f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value));
             break;
           case "select-multiple":
-            j = form.elements[i].options.length - 1;
+            j = f.elements[i].options.length - 1;
             while (j >= 0) {
-              if (form.elements[i].options[j].selected) {
-                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
+              if (f.elements[i].options[j].selected) {
+                q.push(f.elements[i].name + "=" + encodeURIComponent(f.elements[i].options[j].value));
               }
               j = j - 1;
             }
         }
         break;
       case "BUTTON":
-        switch (form.elements[i].type) {
+        switch (f.elements[i].type) {
           case "reset":
           case "submit":
           case "button":
-            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+            q.push(f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value));
         }
     }
     i = i - 1;
--- serialize.js.coffee
+++ serialize.js.coffee
@@ -1,36 +1,36 @@
-window.serialize = (form) ->
-  return if not form or form.nodeName isnt "FORM"
+window.serialize = (f) ->
+  return if not f or f.nodeName isnt "FORM"
   i = undefined
   j = undefined
   q = []
-  i = form.elements.length - 1
+  i = f.elements.length - 1
   while i >= 0
-    if form.elements[i].name is ""
+    if f.elements[i].name is ""
       i = i - 1
       continue
-    switch form.elements[i].nodeName
+    switch f.elements[i].nodeName
       when "INPUT"
-        switch form.elements[i].type
+        switch f.elements[i].type
           when "text", "hidden", "password", "button", "reset", "submit"
-            q.push form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value)
+            q.push f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value)
           when "checkbox", "radio"
-            q.push form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value)  if form.elements[i].checked
+            q.push f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value)  if f.elements[i].checked
           when "file" then break
       when "TEXTAREA"
-        q.push form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value)
+        q.push f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value)
       when "SELECT"
-        switch form.elements[i].type
+        switch f.elements[i].type
           when "select-one"
-            q.push form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value)
+            q.push f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value)
           when "select-multiple"
-            j = form.elements[i].options.length - 1
+            j = f.elements[i].options.length - 1
             while j >= 0
-              q.push form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value)  if form.elements[i].options[j].selected
+              q.push f.elements[i].name + "=" + encodeURIComponent(f.elements[i].options[j].value)  if f.elements[i].options[j].selected
               j = j - 1
       when "BUTTON"
-        switch form.elements[i].type
+        switch f.elements[i].type
           when "reset", "submit", "button"
-            q.push form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value)
+            q.push f.elements[i].name + "=" + encodeURIComponent(f.elements[i].value)
     i = i - 1
   q.join "&"
 
